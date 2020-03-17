b87805ad007d71a3beb4a1d66a466f8d90a3f5f5
Nikita <nikita@Nikitas-MBP.fritz.box>
2020-03-17 09:56:36 +0100

    Add set_up_mac.sh

---
+++ set_up_mac.sh
@@ -0,0 +1,17 @@
+# 1. Install command-line tools:
+echo "===installing COMMAND-LINE TOOLS"
+xcode-select --install
+echo "===COMMAND-LINE TOOLS installed"
+
+# 2. Install homebrew:
+echo "===installing HOMEBREW==="
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo "===HOMEBREW installed==="
+
+# 3. Istall neovim:
+echo "===installing NEOVIM==="
+brew install neovim
+echo "===NEOVIM installed==="
+
+# 4. Install VS Code.
+
