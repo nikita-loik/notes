bb3325c1b06ca80aa4de385e0e7de1ecc0b781f7
Nikita <nikita@Nikitas-MBP.fritz.box>
2020-03-19 10:23:56 +0100

    Update set_up_mac notes

--- set_up_mac.sh
+++ set_up_mac.sh
@@ -4,22 +4,33 @@ xcode-select --install
 # 2. Install homebrew:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# 3. Install neovim:
+# 3. Install python:
+brew install python
+which python
+python --version
+which python3
+python3 --version
+
+# 4. Install virtualenv:
+pip3 install virtualenv
+virtualenv --version
+
+# 4. Install neovim:
 brew install neovim
 
-# 4. Install Java:
+# 5. Install Java:
 # this option may not work
 brew cask install java
 # then use this option
 brew tap adoptopenjdk/openjdk
 brew cask install adoptopenjdk8
 
-# 5. Install Scala:
+# 6. Install Scala:
 brew install scala
 
-# 5. Install Apache Spark:
+# 7. Install Apache Spark:
 brew install apache-spark
 
-# 6. Install VS Code.
-# 7. Install Docker.
+# 8. Install VS Code.
+# 9. Install Docker.
 
