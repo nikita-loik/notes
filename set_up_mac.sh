530a6f13a1a156581bb37aca210ed6b0cb13a419
Nikita Loik <nikita.loik@gmail.com>
2020-04-06 10:29:23 +0200

    Update set_up_mac.sh with java, scala, and spark

--- set_up_mac.sh
+++ set_up_mac.sh
@@ -3,34 +3,48 @@ xcode-select --install
 
 # 2. Install homebrew:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew upgrade && brew update
 
-# 3. Install python:
+# 3. Check Java version:
+# NEW
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk11
+# OLD
+# brew cask install java
+java --version
+# 4. Install Scala:
+brew install scala
+
+# 5. Install Spark:
+brew install apache-spark
+
+# 6. Install python:
 brew install python
 which python
 python --version
 which python3
 python3 --version
 
-# 4. Install virtualenv:
+# 7. Install virtualenv:
 pip3 install virtualenv
 virtualenv --version
 
-# 5. Install neovim:
+# 8. Install neovim:
 brew install neovim
 
-# 6. Install Java:
+# 9. Install Java:
 # this option may not work
 brew cask install java
 # then use this option
 brew tap adoptopenjdk/openjdk
 brew cask install adoptopenjdk8
 
-# 7. Install Scala:
+# 10. Install Scala:
 brew install scala
 
-# 8. Install Apache Spark:
+# 11. Install Apache Spark:
 brew install apache-spark
 
-# 9. Install VS Code.
-# 10. Install Docker.
+# 12. Install VS Code.
+# 13. Install Docker.
 
