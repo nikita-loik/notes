0cb2a28da61d9fee815b3b04db3f794eb288f457
Nikita Loik <nikita@ryans-mbp-2.localdomain>
2020-09-03 08:50:34 +0200

    Update, clean up and rename set_up_venv.sh

--- get_venv.sh
+++ get_venv.sh
@@ -1,26 +1,38 @@
-#!/bin/shell
-# 1. Get virtual environment name.
-# 1.1. Get working directory name (without full path).
+#!/bin/zsh
+
+# CREATE AND ACTIVATE VIRTUAL ENVIRONMENT =============================
+# 1. Get working directory name (without full path).
 dir_name=${PWD##*/}
-# 1.2. Replace '-' with '_' in a name.
+
+# 2. Replace '-' with '_' in a name.
 venv_stem="${dir_name//-/_}"
-# 1.3. Get virtual environment name.
+
+# 3. Get virtual environment name.
 venv_name="venv_${venv_stem}"
 echo "===creating virtual environment ${venv_name}==="
-# 2. Create virtual environment.
+
+# 4. Create virtual environment.
 virtualenv -p python3 ".${venv_name}"
-# 3. Activate virtual environment.
+
+# 5. Activate virtual environment.
 source ".${venv_name}/bin/activate"
-# 4. Install jupyter.
+
+# 6. Upgrade pip.
+pip install -U pip
+
+# JUPYTER =============================================================
 pip install jupyter
-# 5. Install IPython kernel.
+
+# KERNELS =============================================================
+# 1. IPython
 python -m ipykernel install --user --name="${venv_stem}_py"
-# 6. Install scala kernel.
-# 6.1. spylon-kernel (https://pypi.org/project/spylon-kernel/).
+
+# 2 Scala
+# 2.1. spylon-kernel (https://pypi.org/project/spylon-kernel/).
 # NB! seems to work just fine.
 pip install spylon-kernel
 python -m spylon_kernel install --user --name="${venv_stem}_scala"
-# 6.2. almond kernel (https://almond.sh/).
+# 2.2. almond kernel (https://almond.sh/).
 # NB! doesn't work so well.
 # SCALA_VERSION=2.13.1 ALMOND_VERSION=0.9.1
 # curl -Lo coursier https://git.io/coursier-cli
@@ -33,13 +45,16 @@ python -m spylon_kernel install --user --name="${venv_stem}_scala"
 # ./almond --install
 # rm -f almond
 
-# 7. Install requirements.
+# REQUIREMENTS ========================================================
 touch requirements.txt
 pip install -r requirements.txt
 echo "===virtual environment .${venv_name} created==="
+
+# GIT =================================================================
 # 8. If doesn't exist, add virtual environment path to .gitignore.
 grep -qxF ".${venv_name}/" .gitignore || echo ".${venv_name}/" >> .gitignore
 echo "===updated .gitignore with ".${venv_name}/"==="
+
 # 9. Add and commit .gitignore to git.
 # NB Will report 'no changes added to commit' if .gitignore stayed the same.
 git add .gitignore
